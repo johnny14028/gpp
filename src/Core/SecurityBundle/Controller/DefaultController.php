1d1c430c9cc384976694261619425fcf2a51cbf4
Johnny <johnny1402@gmail.com>
2014-11-19 00:15:33 -0500

    habilitar idiomas en el locate

--- src/Core/SecurityBundle/Controller/DefaultController.php
+++ src/Core/SecurityBundle/Controller/DefaultController.php
@@ -3,11 +3,24 @@
 namespace Core\SecurityBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('CoreSecurityBundle:Default:index.html.twig');
+        $request = $this->get('request');
+        //echo $request->getLocale();
+        //$this->vd($this->get('translator'));
+         $translated = $this->get('translator')->trans('Symfony2 is great');
+         //var_dump($translated);
+        //return $this->render('CoreSecurityBundle:Default:index.html.twig');
+         return new Response($translated);
+    }
+    
+    public function vd($var){
+        echo "<pre>";
+        print_r($var);
+        echo "</pre>";
     }
 }
