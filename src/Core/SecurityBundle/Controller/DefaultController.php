d4ea40396f78ff22b55fb47dfb92f8e375aad20d
Johnny <johnny1402@gmail.com>
2014-11-21 00:50:49 -0500

    opciones en la traduccion

--- src/Core/SecurityBundle/Controller/DefaultController.php
+++ src/Core/SecurityBundle/Controller/DefaultController.php
@@ -5,22 +5,28 @@ namespace Core\SecurityBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller
-{
-    public function indexAction()
-    {
+class DefaultController extends Controller {
+
+    public function indexAction() {
         $request = $this->get('request');
         //echo $request->getLocale();
         //$this->vd($this->get('translator'));
-         $translated = $this->get('translator')->trans('Symfony2 is great');
-         //var_dump($translated);
-        //return $this->render('CoreSecurityBundle:Default:index.html.twig');
-         return new Response($translated);
+        $name = 'Susy';
+        //$translated = $this->get('translator')->trans('Hello '.name,array('%name%' => $name));
+        $translated1 = $this->get('translator')->trans('Hello %name%', array('%name%' => $name),'messages');
+        
+$translated = $this->get('translator')->transChoice(
+        'Hay una manzana|Hay %count% manzanas',
+        2,
+        array('%count%' => 2)
+    );        
+        return $this->render('CoreSecurityBundle:Default:index.html.twig', array('text' => $translated, 'name'=>$name, 'saludo'=>$translated1));
     }
-    
-    public function vd($var){
+
+    public function vd($var) {
         echo "<pre>";
         print_r($var);
         echo "</pre>";
     }
+
 }
