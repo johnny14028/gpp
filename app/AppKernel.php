a2ef61ace67327a12844b2561e4d803411634df0
Johnny <johnny1402@gmail.com>
2014-11-22 15:45:57 -0500

    agregar nuevo theme default con bootstrap3

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Core\SecurityBundle\CoreSecurityBundle(),
+            new Theme\DefaultBundle\ThemeDefaultBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
